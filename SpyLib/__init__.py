425a92ccf0f271fe871eda3d48cfd83ab29c34d9
AmanBotz <amanpooniaff@gmail.com>
2025-01-24 08:29:36 +0530

    Add files via upload

--- SpyLib/__init__.py
+++ SpyLib/__init__.py
@@ -3,8 +3,12 @@ import os
 import pathlib
 import time
 import datetime as dt
+
 sys.path.insert(0, f"{pathlib.Path(__file__).parent.resolve()}")
+
 from spylib import upload_file, download_file
+
+
 class Timer:
     def __init__(self, time_between=5):
         self.start_time = time.time()
@@ -15,6 +19,7 @@ class Timer:
             self.start_time = time.time()
             return True
         return False
+
 def progress_bar_str(done, total):
     percent = round(done/total*100, 2)
     strin = "░░░░░░░░░░"
@@ -24,6 +29,7 @@ def progress_bar_str(done, total):
     strin = "".join(strin)
     final = f"Percent: {percent}%\n{human_readable_size(done)}/{human_readable_size(total)}\n{strin}"
     return final 
+
 def human_readable_size(size, decimal_places=2):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
         if size < 1024.0 or unit == 'PB':
@@ -31,15 +37,16 @@ def human_readable_size(size, decimal_places=2):
         size /= 1024.0
     return f"{size:.{decimal_places}f} {unit}"
 
-async def fast_download(client, msg, reply = None, download_folder = None, progress_bar_function = progress_bar_str):
+async def fast_download(client, msg, reply = None, download_folder = None, progress_bar_function = progress_bar_str, name = None):
     timer = Timer()
 
     async def progress_bar(downloaded_bytes, total_bytes):
         if timer.can_send():
             data = progress_bar_function(downloaded_bytes, total_bytes)
             await reply.edit(f"Downloading...\n{data}")
+
     file = msg.document
-    filename = msg.file.name
+    filename = name if name else msg.file.name
     dir = "downloads/"
 
     try:
@@ -94,8 +101,5 @@ async def fast_upload(client, file_location, reply=None, name=None, progress_bar
                 file=f,
                 name=name,
             )
-
-
-
         
     return the_file
